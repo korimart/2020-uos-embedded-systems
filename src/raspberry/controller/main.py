62bb946a61fe51ac25c4c413be9d317a65028727
korimart <korimart2019@gmail.com>
2020-11-23 16:39:49 +0900

    뻘짓

--- src/raspberry/controller/main.py
+++ src/raspberry/controller/main.py
@@ -8,28 +8,35 @@ os.chdir(THISDIR)
 import EasyPygame
 from EasyPygame.Components import *
 from OpenGL.GL import *
+from CarController import CarController
 
 class Scene1(Scene):
     def __init__(self):
         super().__init__()
         self.carrot = None
+        self.carController = None
 
     def onLoad(self):
         self.carrot = GameObject(self, "Carrot")
         self.carrot.renderComp = DefaultRenderComponent()
+        self.carController = CarController()
 
     def preRender(self, ms):
         if EasyPygame.isDown("LEFT"):
-            self.carrot.transform.translate(-0.1, 0, 0)
+            self.carController.onLeft(ms)
+            # self.carrot.transform.translate(-0.1, 0, 0)
 
         if EasyPygame.isDown("RIGHT"):
-            self.carrot.transform.translate(0.1, 0, 0)
+            self.carController.onRight(ms)
+            # self.carrot.transform.translate(0.1, 0, 0)
 
         if EasyPygame.isDown("UP"):
-            self.carrot.transform.translate(0, 0.1, 0)
+            self.carController.onUp(ms)
+            # self.carrot.transform.translate(0, 0.1, 0)
 
         if EasyPygame.isDown("DOWN"):
-            self.carrot.transform.translate(0, -0.1, 0)
+            self.carController.onDown(ms)
+            # self.carrot.transform.translate(0, -0.1, 0)
 
 if __name__ == "__main__":
     EasyPygame.initWindow(500, 500, "Sample", 75)
