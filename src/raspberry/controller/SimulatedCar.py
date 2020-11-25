687047e190a6b36a9c68f3b2cf403ec4d4b1563d
korimart <korimart2019@gmail.com>
2020-11-25 16:40:58 +0900

    added new method to EasyPyGame

--- src/raspberry/controller/SimulatedCar.py
+++ src/raspberry/controller/SimulatedCar.py
@@ -3,24 +3,23 @@ from EasyPygame.Components.GameObject import GameObject
 class SimulatedCar():
     def __init__(self, carGameObject, width=1):
         self.carGameObject : GameObject = carGameObject
-        self.left_wheel = 0
-        self.right_wheel = 0
+        self.leftSpeed = 0
+        self.rightSpeed = 0
         self.width = width
 
     def update(self, ms):
-        dist = self.left_wheel * ms / 1000
-        self.carGameObject.transform.translate(0, dist, 0)
+        pass
 
     def finish_iteration(self):
         print('finish iteration')
 
     def set_right_speed(self, speed):
         print('set right speed to ', speed)
-        self.right_wheel = speed
+        self.rightSpeed = speed
     
     def set_left_speed(self, speed):
         print('set left speed to ', speed)
-        self.left_wheel = speed
+        self.leftSpeed = speed
         
     def get_image_from_camera(self):
         pass
