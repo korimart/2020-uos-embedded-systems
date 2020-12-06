af79617812ef2691863cfa787207570128c01e84
korimart <34030222+korimart@users.noreply.github.com>
2020-12-06 17:44:16 +0900

    test

--- src/raspberry/controller/CarImp/RealCar.py
+++ src/raspberry/controller/CarImp/RealCar.py
@@ -25,12 +25,14 @@ class RealCar(Car):
         print("My cmd is %s" % cmd)
         self.ser.write(cmd)
         print("return was {}".format(self.ser.read(1)))
+        print("return was {}".format(self.ser.read(1)))
 
     def set_left_speed(self, speed):
         print("set left speed to ", speed)
         cmd = ("L%d\n" % speed).encode("ascii")
         print("My cmd is %s" % cmd)
         self.ser.write(cmd)
+        print("return was {}".format(self.ser.read(1)))
         print("return was {}".format(self.ser.read(1)))
 
     def stop(self):  # robot stop
