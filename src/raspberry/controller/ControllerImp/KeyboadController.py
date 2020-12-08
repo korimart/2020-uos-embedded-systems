078223bf629c975c22c1068c51cb947cf4e16480
korimart <korimart2019@gmail.com>
2020-12-08 16:15:54 +0900

    hello

--- src/raspberry/controller/ControllerImp/KeyboadController.py
+++ src/raspberry/controller/ControllerImp/KeyboadController.py
@@ -1,4 +1,5 @@
 from ..Car import Car
+import time
 
 class KeyboardController:
     def __init__(self, car) -> None:
@@ -7,31 +8,31 @@ class KeyboardController:
         self.rightRolling = False
     
     def goStraight(self):
-        if not self.leftRolling:
-            self.leftRolling = True
-            self.car.set_left_speed(2)
-        if not self.rightRolling:
-            self.rightRolling = True
-            self.car.set_right_speed(2)
+        self.leftRolling = True
+        self.rightRolling= True
+        self.car.set_left_speed(2)
+        self.car.set_right_speed(2)
+        time.sleep(0.5)
+        self.car.set_left_speed(0)
+        self.car.set_right_speed(0)
+        
     
     def stop(self):
-        self.leftRolling = False
-        self.rightRolling = False
         self.car.set_left_speed(0)
         self.car.set_right_speed(0)
 
     def goLeft(self, speed):
-        self.leftRolling = False
         self.car.set_left_speed(0)
-
-        if not self.rightRolling:
-            self.rightRolling = True
-            self.car.set_right_speed(speed)
-
-    def goRight(self, speed):
-        self.rightRolling = False
+        self.car.set_right_speed(speed)
+        time.sleep(0.5)
         self.car.set_right_speed(0)
-
-        if not self.leftRolling:
-            self.leftRolling = True
-            self.car.set_left_speed(speed)
+        self.car.set_left_speed(0)
+   
+        
+    def goRight(self, speed):
+        
+        self.car.set_left_speed(speed)
+        self.car.set_right_speed(0)
+        time.sleep(0.5)
+        self.car.set_left_speed(0)
+        self.car.set_right_speed(0)        
--- src/raspberry/socket-test/rasp.py
+++ src/raspberry/socket-test/rasp.py
@@ -1,17 +1,37 @@
 import socket
 from ..controller.CarImp.RealCar import RealCar
 from ..controller.CarCameraImp import RealCamera
-from ..controller.ControllerImp.KeyboadController import KeyboardController
 
 car = RealCar(RealCamera())
-controller = KeyboardController(car)
 
 # create an INET, STREAMing socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # now connect to the web server on port 80 - the normal http port
 s.connect(("192.168.137.1", 8080))
-receivedBytes = s.recv(100)
 
-if receivedBytes == b"goStraight":
-    controller.goStraight()
+while True:
     
+    receivedBytes = s.recv(100) 
+    if receivedBytes :
+        print(receivedBytes)
+        
+        if receivedBytes == b"C":
+            img = car.get_image_from_camera()
+            img = img.reshape(16*16)
+            msg = img.tobytes()
+            s.send(msg)
+            
+        
+        if receivedBytes == b"F":
+            car.set_right_speed(2)
+            car.set_left_speed(2)
+
+        if recievedBytes == b"L":
+            car.set_right_speed(2)
+
+        if recievedBytes == b"R":
+            car.set_right_speed(2)
+            
+        if recievedBytes == b"S":
+            car.set_left_speed(0)
+            car.set_right_speed(0)
