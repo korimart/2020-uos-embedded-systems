d29a6e3349c7ca4b5374979eb5fc58006426a305
korimart <34030222+korimart@users.noreply.github.com>
2020-12-08 18:50:36 +0900

    handle connection lost

--- src/raspberry/socket-rasp.py
+++ src/raspberry/socket-rasp.py
@@ -2,40 +2,46 @@ import socket
 from controller.CarImp.RealCar import RealCar
 from controller.CarCameraImp.RealCamera import RealCamera
 
-car = RealCar(RealCamera())
+def initSocket():
+    # create an INET, STREAMing socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # now connect to the web server on port 80 - the normal http port
+    s.connect(("192.168.137.1", 8080))
+    return s
 
-# create an INET, STREAMing socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# now connect to the web server on port 80 - the normal http port
-s.connect(("192.168.137.1", 8080))
+car = RealCar(RealCamera())
+s = initSocket()
 
 while True:
-    
-    receivedBytes = s.recv(2) 
-    if receivedBytes :
-        receivedBytes = receivedBytes.decode("ascii")
-        print(receivedBytes)
-        
-        if receivedBytes[0] == "C":
-            img = car.get_image_from_camera()
-            img = img.reshape(16*16)
-            msg = img.tobytes()
-            s.send(msg)
-            
-        
-        if receivedBytes[0] == "F":
-            car.set_right_speed(2)
-            car.set_left_speed(2)
+    try:
+        receivedBytes = s.recv(2) 
 
-        if receivedBytes[0] == "L":
-            speed=int(receivedBytes[1])
+        if receivedBytes:
+            receivedBytes = receivedBytes.decode("ascii")
+            print(receivedBytes)
             
-            car.set_left_speed(speed)
+            if receivedBytes[0] == "C":
+                img = car.get_image_from_camera()
+                img = img.reshape(16*16)
+                msg = img.tobytes()
+                s.send(msg)
+                
+            if receivedBytes[0] == "F":
+                car.set_right_speed(2)
+                car.set_left_speed(2)
 
-        if receivedBytes[0] == "R":
-            speed=int(receivedBytes[1])
-            car.set_right_speed(speed)
-            
-        if receivedBytes[0] == "S":
-            car.set_left_speed(0)
-            car.set_right_speed(0)
+            if receivedBytes[0] == "L":
+                speed=int(receivedBytes[1])
+                
+                car.set_left_speed(speed)
+
+            if receivedBytes[0] == "R":
+                speed=int(receivedBytes[1])
+                car.set_right_speed(speed)
+                
+            if receivedBytes[0] == "S":
+                car.set_left_speed(0)
+                car.set_right_speed(0)
+
+    except ConnectionResetError:
+        s = initSocket()
