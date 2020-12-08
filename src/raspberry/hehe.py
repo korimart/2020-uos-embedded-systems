4e896d67e9492b2999b52743dfd7ba45574ae9a5
korimart <korimart2019@gmail.com>
2020-12-09 01:35:42 +0900

    trained-model3 0.05

--- src/raspberry/hehe.py
+++ src/raspberry/hehe.py
@@ -2,7 +2,7 @@ from neural.DataLoaderImp.OurDataLoader import OurDataLoader
 from neural.ModelImp.OurModel import OurModel
 import numpy as np
 
-loader = OurDataLoader("our-data-R90.npy")
+loader = OurDataLoader("our-data-R90.npy", ratio=1)
 model = OurModel()
 model.load("trained-model3.p") 
 
