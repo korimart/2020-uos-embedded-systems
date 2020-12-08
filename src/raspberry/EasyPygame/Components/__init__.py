849861f0e40f0525edb6feffecdb914594ad186a
korimart <korimart2019@gmail.com>
2020-12-08 18:32:02 +0900

    really removed EasyPygame

--- src/raspberry/EasyPygame/Components/__init__.py
+++
@@ -1,5 +0,0 @@
-from EasyPygame.Components.GameObject import *
-from EasyPygame.Components.RenderComp import *
-from EasyPygame.Components.TransformComp import TransformComp
-from EasyPygame.Components.Scene import Scene, DefaultScene
-from EasyPygame.Components import GUI
