f538035eae1b94ef924a06bba7b4900c732e326e
korimart <korimart2019@gmail.com>
2020-12-08 18:36:25 +0900

    rasp final

--- src/raspberry/socket-rasp.py
+++ src/raspberry/socket-rasp.py
@@ -11,27 +11,31 @@ s.connect(("192.168.137.1", 8080))
 
 while True:
     
-    receivedBytes = s.recv(100) 
+    receivedBytes = s.recv(2) 
     if receivedBytes :
+        receivedBytes = receivedBytes.decode("ascii")
         print(receivedBytes)
         
-        if receivedBytes == b"C":
+        if receivedBytes[0] == "C":
             img = car.get_image_from_camera()
             img = img.reshape(16*16)
             msg = img.tobytes()
             s.send(msg)
             
         
-        if receivedBytes == b"F":
+        if receivedBytes[0] == "F":
             car.set_right_speed(2)
             car.set_left_speed(2)
 
-        if receivedBytes == b"L":
-            car.set_right_speed(2)
-
-        if receivedBytes == b"R":
-            car.set_right_speed(2)
+        if receivedBytes[0] == "L":
+            speed=int(receivedBytes[1])
             
-        if receivedBytes == b"S":
+            car.set_left_speed(speed)
+
+        if receivedBytes[0] == "R":
+            speed=int(receivedBytes[1])
+            car.set_right_speed(speed)
+            
+        if receivedBytes[0] == "S":
             car.set_left_speed(0)
             car.set_right_speed(0)
