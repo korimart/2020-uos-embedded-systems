4fd3a96a4597f838d4a07c5979e25fc4dbf83e1e
korimart <34030222+korimart@users.noreply.github.com>
2020-12-08 15:31:20 +0900

    rasp sample

--- src/raspberry/socket-test/rasp.py
+++ src/raspberry/socket-test/rasp.py
@@ -1,8 +1,17 @@
 import socket
+from ..controller.CarImp.RealCar import RealCar
+from ..controller.CarCameraImp import RealCamera
+from ..controller.ControllerImp.KeyboadController import KeyboardController
+
+car = RealCar(RealCamera())
+controller = KeyboardController(car)
 
 # create an INET, STREAMing socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # now connect to the web server on port 80 - the normal http port
 s.connect(("192.168.137.1", 8080))
 receivedBytes = s.recv(100)
-print(receivedBytes)
+
+if receivedBytes == b"goStraight":
+    controller.goStraight()
+    
